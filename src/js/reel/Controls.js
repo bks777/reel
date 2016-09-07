60da534e30a4aef05f705897417a146495887263
k.bokov <k.bokov@lucky-labs.com>
2016-09-07 18:27:01 +0300

    temp commit

--- src/js/reel/Controls.js
+++ src/js/reel/Controls.js
@@ -1,6 +1,15 @@
 const PIXI = window.PIXI;
 import config from './config';
+
 export default class Controls extends PIXI.Container{
+    /**
+     * Init of buttons
+     * @param startBtnCallback
+     * @param stopBtnCallback
+     * @param aboutBtnCallbak
+     * @extends PIXI.Container
+     * @constructor
+     */
     constructor(startBtnCallback, stopBtnCallback, aboutBtnCallbak){
         super();
 
@@ -9,9 +18,13 @@ export default class Controls extends PIXI.Container{
         this.position = new PIXI.Point(config.controls.x, config.controls.y);
         this._addStartButton(startBtnCallback);
         this._addStopButton(stopBtnCallback);
-        this._addAboutButton(aboutBtnCallbak);
     }
 
+    /**
+     * Inits and adds a START button
+     * @param callback
+     * @private
+     */
     _addStartButton(callback) {
         let btnContainer = new PIXI.Container(),
             btnTxt = new PIXI.Text(
@@ -45,6 +58,11 @@ export default class Controls extends PIXI.Container{
         this.addChild(btnContainer);
     }
 
+    /**
+     * Inits and adds a STOP button
+     * @param callback
+     * @private
+     */
     _addStopButton(callback) {
         let btnContainer = new PIXI.Container(),
             btnTxt = new PIXI.Text(
@@ -78,36 +96,12 @@ export default class Controls extends PIXI.Container{
         this.addChild(btnContainer);
     }
 
-    _addAboutButton(callback) {
-        let btnContainer = new PIXI.Container(),
-            btnTxt = new PIXI.Text(
-                config.controls.aboutBtn.text,
-                config.controls.aboutBtn.textStyle
-            ),
-            btnBg = new PIXI.Graphics();
-        btnBg.beginFill(config.controls.btn.color, 1);
-        btnBg.drawRoundedRect(
-            0,
-            0,
-            config.controls.btn.width,
-            config.controls.btn.height,
-            config.controls.btn.cornersRadius
-        );
-        btnBg.endFill();
-        btnBg = new PIXI.Sprite(btnBg.generateCanvasTexture());
+    /**
+     * Inits a button object with data
+     * @param config
+     * @private
+     */
+    _createButton(config){
 
-        btnTxt.x = (config.controls.btn.width - btnTxt.width) / 2;
-        btnTxt.y = (config.controls.btn.height - btnTxt.height) / 2;
-
-        btnContainer.addChild(btnBg);
-        btnContainer.addChild(btnTxt);
-
-        btnContainer.interactive = btnContainer.buttonMode = true;
-        btnContainer.defaultCursor = 'pointer';
-        btnContainer.click = btnContainer.touchend = callback;
-
-        btnContainer.position = new PIXI.Point(config.controls.aboutBtn.x, config.controls.aboutBtn.y)
-
-        this.addChild(btnContainer);
     }
 }
